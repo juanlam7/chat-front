b1543788d1c833d97847d71d035d702db907e691
juanlam7 <juanlam7@gmail.com>
2024-12-14 15:36:38 +0100

    feat: Add needed hook to retrieve data

---
+++ src/hooks/useLogout.ts
@@ -0,0 +1,33 @@
+import { useAuthContext } from "@/context/AuthContext";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+const useLogout = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const logout = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch("/api/auth/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      localStorage.removeItem("chat-user");
+      setAuthUser(null);
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      toast.error((error as any).message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, logout };
+};
+export default useLogout;
---
+++ src/zustand/useConversation.ts
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+
+interface Message {
+  _id: string;
+  senderId: string;
+  receiverId: string;
+  message: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Conversation {
+  _id: string;
+  fullName: string;
+  username: string;
+  password: string;
+  gender: "male" | "female";
+  profilePic: string;
+}
+
+interface ConversationStore {
+  selectedConversation: Conversation | null;
+  setSelectedConversation: (selectedConversation: Conversation | null) => void;
+  messages: Message[];
+  setMessages: (messages: Message[]) => void;
+}
+
+const useConversation = create<ConversationStore>((set) => ({
+  selectedConversation: null,
+  setSelectedConversation: (selectedConversation) =>
+    set({ selectedConversation }),
+  messages: [],
+  setMessages: (messages) => set({ messages }),
+}));
+
+export default useConversation;
